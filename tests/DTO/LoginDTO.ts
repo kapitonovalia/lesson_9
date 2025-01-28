5e589ad5f04b997df897bb49893c7c5344e571e7
AndrewKhv <a.d.khramchenkov@gmail.com>
2025-01-28 18:57:52 +0100

    feat: TL-11 added login test

---
+++ tests/DTO/LoginDTO.ts
@@ -0,0 +1,16 @@
+export class LoginDTO {
+  username: string;
+  password: string;
+
+  private constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
+
+  static createLoginWithCorrectData(): LoginDTO {
+    return new LoginDTO(
+      process.env.USERNAME || "",
+      process.env.PASSWORD || ""
+    );
+  }
+}
