ca869538dfb49ea731cd643b8400c752fb4f75a7
AndrewKhv <a.d.khramchenkov@gmail.com>
2025-01-28 19:02:08 +0100

    feat: TL-11 prettier fixes

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -2,13 +2,13 @@ import { expect, test } from '@playwright/test'
 import { LoginDTO } from './DTO/LoginDTO'
 import { StatusCodes } from 'http-status-codes'
 
-test.describe("Login tests", async () => {
+test.describe('Login tests', async () => {
   test('Successful authorization', async ({ request }) => {
     const response = await request.post(`https://backend.tallinn-learning.ee/login/student`, {
-      data: LoginDTO.createLoginWithCorrectData()
-    });
+      data: LoginDTO.createLoginWithCorrectData(),
+    })
 
-    console.log((await response.text()));
-    expect(response.status()).toBe(StatusCodes.OK);
+    console.log(await response.text())
+    expect(response.status()).toBe(StatusCodes.OK)
   })
 })
